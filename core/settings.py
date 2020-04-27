caedb2c585ce26f8cd9a2b94d6e3fe4eea04a2c4
shantanu <mailvishwanadha@gmail.com>
2020-04-27 20:05:38 +0530

    1

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-dashboard-black-pro.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gentle-dawn-55055.herokuapp.com/']
 
 # Application definition
 
